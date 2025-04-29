303e0b0a15e404f3de0709d234618b00b89a1d7d
Juarez Junior <112733205+Jjunior112@users.noreply.github.com>
2025-04-29 09:31:48 -0300

    Update UnitOfWork.cs

--- infrastructure/persistence/UnitOfWork.cs
+++ infrastructure/persistence/UnitOfWork.cs
@@ -1,16 +1,22 @@
 public class UnitOfWork : IUnitOfWork
 {
     private readonly TaskDbContext _context;
-    public IRepository TaskRepository { get; }
 
-    public UnitOfWork(TaskDbContext context, IRepository taskRepository)
+    public ITaskRepository TaskRepository { get; }
+
+    public UnitOfWork(TaskDbContext context, ITaskRepository taskRepository)
     {
         _context = context;
         TaskRepository = taskRepository;
     }
 
-    public async Task CommitAsync()
+    public async Task<int> CommitAsync()
     {
-        await _context.SaveChangesAsync();
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
     }
 }
